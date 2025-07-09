3373eb539be0ba0f3e0dc492c0125f1db9171c5e
david <leachda12@icloud.com>
2025-07-09 19:10:45 -0400

    Refactor CardsController to implement Elasticsearch querying

--- server/Controllers/CardsController.cs
+++ server/Controllers/CardsController.cs
@@ -1,3 +1,7 @@
+using System.Threading.Tasks;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.Core.Reindex;
+using Elastic.Clients.Elasticsearch.Nodes;
 using Microsoft.AspNetCore.Mvc;
 using MTGShoebox.Models;
 
@@ -8,20 +12,37 @@ namespace MTGShoebox.Controllers
     [Route("api/[controller]")]
     public class CardsController : ControllerBase
     {
-        [HttpGet]
-        // TODO: Change to ActionResult<List<Card>> once the Card model is made
-        public ActionResult<List<Card>> Get([FromQuery] string q)
+        private ElasticsearchClient _client;
+
+        public CardsController(ElasticsearchClient client)
         {
-            // TODO: Set up for Elastic querying
-            List<Card> testData = new List<Card>
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Card>>> Get([FromQuery] string q)
+        {
+            var response = await _client.SearchAsync<Card>(search => search
+                .Indices("mtg_cards")
+                .Query(query => query
+                    .Match(match => match
+                        .Field(field => field.Name)
+                            .Query("bolt")
+                    )
+                )
+                .Size(10) // TODO: Adjust the size once pagination is implemented
+            );
+
+            Console.WriteLine("Results:");
+            foreach (var hit in response.Hits)
             {
-                new Card { Name = "Lightning Bolt", ManaCost = "{R}", Type = "Instant" },
-                new Card { Name = "Black Lotus", ManaCost = "{0}", Type = "Artifact" },
-                new Card { Name = "Sol Ring", ManaCost = "{1}", Type = "Artifact"}
-            };
+                Console.WriteLine($"Card Name: {hit.Source.Name} - Type: {hit.Source.Type} - Mana Cost: {hit.Source.ManaCost}");
+            }
+
+            var cards = response.Hits.Select(hit => hit.Source).ToList();
 
             // Returns a JSON formatted body with an HTTP 200 OK status
-            return Ok(testData);
+            return Ok(cards);
         }
     }
 }
